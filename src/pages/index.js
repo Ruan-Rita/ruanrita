e81956e9cf213a827c40c748049da96b93f3a9d6
Ruan Rita <54330181+Ruan-Rita@users.noreply.github.com>
2024-01-10 22:35:43 -0300

    feat: add tab for projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,8 +51,137 @@ export default function Home() {
               <ChevronDoubleRightIcon className="text-white" width={20}/>
             </div>
           </div>
+          <section className="my-16 grid grid-cols-3 gap-8">
+            <div className="border-2 border-gray">
+              <img src="/RuanRita/project_1.png" className="w-full h-[230px] object-cover block" />
+              <p className="p-4 border-b-2 text-gray border-gray">HTML CSS JS PHP LARAVEL TAILwind</p>  
+              <div className="p-4">
+                <h1 className="text-white font-bold">ChertNodes</h1>
+                <p className="text-white min-h-14">minecraft servers hosting</p>
+                <div>
+                  <button className="border w-24 mr-4 border-primary px-5 py-2 text-white">Live</button>
+                  <button className="border w-24 border-gray px-5 py-2 text-white">Cached</button>
+                </div>
+              </div>
+            </div>
+            <div className="border-2 border-gray">
+              <img src="/RuanRita/project_2.png" className="w-full h-[230px] object-cover block" />
+              <p className="p-4 border-b-2 text-gray border-gray">HTML CSS JS PHP LARAVEL TAILwind</p>  
+              <div className="p-4">
+                <h1 className="text-white font-bold">ChertNodes</h1>
+                <p className="text-white min-h-14">minecraft servers hosting</p>
+                <div>
+                  <button className="border w-24 mr-4 border-primary px-5 py-2 text-white">Live</button>
+                  <button className="border w-24 border-gray px-5 py-2 text-white">Cached</button>
+                </div>
+              </div>
+            </div>
+            <div className="border-2 border-gray">
+              <img src="/RuanRita/project_3.jpg" className="w-full h-[230px] object-cover block" />
+              <p className="p-4 border-b-2 text-gray border-gray">HTML CSS JS PHP LARAVEL TAILwind</p>  
+              <div className="p-4">
+                <h1 className="text-white font-bold">ChertNodes</h1>
+                <p className="text-white min-h-14">minecraft servers hosting</p>
+                <div>
+                  <button className="border w-24 mr-4 border-primary px-5 py-2 text-white">Live</button>
+                  <button className="border w-24 border-gray px-5 py-2 text-white">Cached</button>
+                </div>
+              </div>
+            </div>
+          </section>
         </section>
-
+        {/* My skills */}
+        <section className="my-16">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <h1 className="text-white text-2xl font-bold mr-12"><b className="text-primary">#</b> Skills</h1>
+              <hr className="bg-primary border-primary w-80"></hr>
+            </div>
+            <div className="flex items-center">
+              <h1 className="text-white mr-2">View all</h1>
+              <ChevronDoubleRightIcon className="text-white" width={20}/>
+            </div>
+          </div>
+          <section className="h-[500px] flex justify-between">
+            <div></div>
+            <div className="grid grid-cols-3">
+              <div className="p-3">
+                <h1>Languages</h1>
+                <p>Typescript JavaScript</p>
+                <p>PHP</p>
+              </div>
+              <div>
+                <h1>Databases</h1>
+                <p>SQLite PostgreSQL</p>
+                <p>Mongo MySQL</p>
+              </div>
+              <div>
+                <h1>Tools</h1>
+                <p>VSCode Git</p>
+                <p>Tailwind </p>
+              </div>
+            </div>
+          </section>
+        </section>
+        {/* About me*/}
+        <section className="my-16">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <h1 className="text-white text-2xl font-bold mr-12"><b className="text-primary">#</b> About-me</h1>
+              <hr className="bg-primary border-primary w-80"></hr>
+            </div>
+            <div className="flex items-center">
+              <h1 className="text-white mr-2">View all</h1>
+              <ChevronDoubleRightIcon className="text-white" width={20}/>
+            </div>
+          </div>
+          <section className="h-[500px] flex justify-between">
+            <div></div>
+            <div className="grid grid-cols-3">
+              <div className="p-3">
+                <h1>Languages</h1>
+                <p>Typescript JavaScript</p>
+                <p>PHP</p>
+              </div>
+              <div>
+                <h1>Databases</h1>
+                <p>SQLite PostgreSQL</p>
+                <p>Mongo MySQL</p>
+              </div>
+              <div>
+                <h1>Tools</h1>
+                <p>VSCode Git</p>
+                <p>Tailwind </p>
+              </div>
+            </div>
+          </section>
+        </section>
+        {/* contacts*/}
+        <section className="my-16">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <h1 className="text-white text-2xl font-bold mr-12"><b className="text-primary">#</b> Contacts</h1>
+              <hr className="bg-primary border-primary w-80"></hr>
+            </div>
+            <div className="flex items-center">
+              <h1 className="text-white mr-2">View all</h1>
+              <ChevronDoubleRightIcon className="text-white" width={20}/>
+            </div>
+          </div>
+          <section className="h-[500px] flex justify-between">
+            <p>I’m interested in freelance opportunities. However, if you have other request or question, don’t hesitate to contact me</p>
+            <div className="border border-gray p-4">
+              <h1>Message me here</h1>
+              <span className="block">
+                <p>ruan.ritah</p>
+              </span>
+              <span className="block">
+                <p>ruan.ritah@gamil.com</p>
+              </span>
+            </div>
+          </section>
+        </section>
+          
       </div>
     </main>
   );
