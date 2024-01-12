71230552020483af339e84af00f84aa35184e2d7
Ruan Rita <54330181+Ruan-Rita@users.noreply.github.com>
2024-01-11 22:00:14 -0300

    feat: raw landing is almost finish

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { ChevronDoubleRightIcon } from "@heroicons/react/24/outline";
+import { ChevronDoubleRightIcon, EnvelopeIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import NavBar from "../components/Navbar/navbar";
 export default function Home() {
@@ -51,7 +51,7 @@ export default function Home() {
               <ChevronDoubleRightIcon className="text-white" width={20}/>
             </div>
           </div>
-          <section className="my-16 grid grid-cols-3 gap-8">
+          <section className="mt-10 grid grid-cols-3 gap-8">
             <div className="border-2 border-gray">
               <img src="/RuanRita/project_1.png" className="w-full h-[230px] object-cover block" />
               <p className="p-4 border-b-2 text-gray border-gray">HTML CSS JS PHP LARAVEL TAILwind</p>  
@@ -92,7 +92,7 @@ export default function Home() {
         </section>
         {/* My skills */}
         <section className="my-16">
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between mb-10">
             <div className="flex items-center">
               <h1 className="text-white text-2xl font-bold mr-12"><b className="text-primary">#</b> Skills</h1>
               <hr className="bg-primary border-primary w-80"></hr>
@@ -102,30 +102,49 @@ export default function Home() {
               <ChevronDoubleRightIcon className="text-white" width={20}/>
             </div>
           </div>
-          <section className="h-[500px] flex justify-between">
-            <div></div>
-            <div className="grid grid-cols-3">
-              <div className="p-3">
-                <h1>Languages</h1>
-                <p>Typescript JavaScript</p>
-                <p>PHP</p>
+          <section className="flex justify-between">
+            <div className="w-1/2">
+              <img src="/RuanRita/developer.webp" className="w-full object-contain max-h-72 object-left"/>
+            </div>
+            <div className="grid grid-cols-3 gap-3">
+              <div className="border border-gray h-min">
+                <h1 className="text-white border-b border-gray p-2">Languages</h1>
+                <p className="p-2 text-white">Typescript <span className="mx-2"></span> JavaScript</p>
+                <p className="p-2 text-white">PHP</p>
               </div>
               <div>
-                <h1>Databases</h1>
-                <p>SQLite PostgreSQL</p>
-                <p>Mongo MySQL</p>
+                <div className="border border-gray h-min mb-3">
+                  <h1 className="text-white border-b border-gray p-2">Databases</h1>
+                  <p className="p-2 text-white">SQLite PostgreSQL</p>
+                  <p className="p-2 text-white">Mongo MySQL</p>
+                  <p className="p-2 text-white">Mongo MySQL</p>
+                </div>
+                <div className="border border-gray h-min mb-3">
+                  <h1 className="text-white border-b border-gray p-2">Databases</h1>
+                  <p className="p-2 text-white">SQLite PostgreSQL</p>
+                  <p className="p-2 text-white">Mongo MySQL</p>
+                </div>
               </div>
               <div>
-                <h1>Tools</h1>
-                <p>VSCode Git</p>
-                <p>Tailwind </p>
+                <div className="border border-gray h-min mb-3">
+                  <h1 className="text-white border-b border-gray p-2">Tools</h1>
+                  <p className="p-2 text-white">VSCode Git</p>
+                  <p className="p-2 text-white">Tailwind </p>
+                  <p className="p-2 text-white">Tailwind </p>
+                  <p className="p-2 text-white">Tailwind </p>
+                </div>
+                <div className="border border-gray h-min mb-3">
+                  <h1 className="text-white border-b border-gray p-2">Tools</h1>
+                  <p className="p-2 text-white">VSCode Git</p>
+                  <p className="p-2 text-white">Tailwind </p>
+                </div>
               </div>
             </div>
           </section>
         </section>
         {/* About me*/}
         <section className="my-16">
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between mb-10">
             <div className="flex items-center">
               <h1 className="text-white text-2xl font-bold mr-12"><b className="text-primary">#</b> About-me</h1>
               <hr className="bg-primary border-primary w-80"></hr>
@@ -135,30 +154,20 @@ export default function Home() {
               <ChevronDoubleRightIcon className="text-white" width={20}/>
             </div>
           </div>
-          <section className="h-[500px] flex justify-between">
-            <div></div>
-            <div className="grid grid-cols-3">
-              <div className="p-3">
-                <h1>Languages</h1>
-                <p>Typescript JavaScript</p>
-                <p>PHP</p>
-              </div>
-              <div>
-                <h1>Databases</h1>
-                <p>SQLite PostgreSQL</p>
-                <p>Mongo MySQL</p>
-              </div>
-              <div>
-                <h1>Tools</h1>
-                <p>VSCode Git</p>
-                <p>Tailwind </p>
-              </div>
+          <section className="flex justify-between gap-5">
+            <div className="w-1/2">
+              <p className="text-gray tracking-widest leading-6">Hello, i’m Elias!</p>
+              <p className="text-gray tracking-widest leading-6 my-6">I’m a self-taught front-end developer based in Kyiv, Ukraine. I can develop responsive websites from scratch and raise them into modern user-friendly web experiences.</p>
+              <p className="text-gray tracking-widest leading-6">Transforming my creativity and knowledge into a websites has been my passion for over a year. I have been helping various clients to establish their presence online. I always strive to learn about the newest technologies and frameworks.</p>
+            </div>
+            <div className="w-1/2">
+              <img src="/RuanRita/developer_1.webp" className="w-full object-contain max-h-72 object-right"/>
             </div>
           </section>
         </section>
         {/* contacts*/}
         <section className="my-16">
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between mb-10">
             <div className="flex items-center">
               <h1 className="text-white text-2xl font-bold mr-12"><b className="text-primary">#</b> Contacts</h1>
               <hr className="bg-primary border-primary w-80"></hr>
@@ -168,21 +177,60 @@ export default function Home() {
               <ChevronDoubleRightIcon className="text-white" width={20}/>
             </div>
           </div>
-          <section className="h-[500px] flex justify-between">
-            <p>I’m interested in freelance opportunities. However, if you have other request or question, don’t hesitate to contact me</p>
-            <div className="border border-gray p-4">
-              <h1>Message me here</h1>
-              <span className="block">
+          <section className="flex justify-between">
+            <p className="w-1/2 text-gray tracking-widest leading-6 mb-6">I’m interested in freelance opportunities. However, if you have other request or question, don’t hesitate to contact me</p>
+            <div className="border border-gray h-min">
+              <h1 className="p-2 text-white">Message me here</h1>
+              <span className="p-2 text-gray flex align-middle">
+                <svg width="32px" height="32px" viewBox="0 -28.5 256 256" xmlns="http://www.w3.org/2000/svg"  className="mr-2 fill-gray">
+                  <g>
+                    <path d="M216.856339,16.5966031 C200.285002,8.84328665 182.566144,3.2084988 164.041564,0 C161.766523,4.11318106 159.108624,9.64549908 157.276099,14.0464379 C137.583995,11.0849896 118.072967,11.0849896 98.7430163,14.0464379 C96.9108417,9.64549908 94.1925838,4.11318106 91.8971895,0 C73.3526068,3.2084988 55.6133949,8.86399117 39.0420583,16.6376612 C5.61752293,67.146514 -3.4433191,116.400813 1.08711069,164.955721 C23.2560196,181.510915 44.7403634,191.567697 65.8621325,198.148576 C71.0772151,190.971126 75.7283628,183.341335 79.7352139,175.300261 C72.104019,172.400575 64.7949724,168.822202 57.8887866,164.667963 C59.7209612,163.310589 61.5131304,161.891452 63.2445898,160.431257 C105.36741,180.133187 151.134928,180.133187 192.754523,160.431257 C194.506336,161.891452 196.298154,163.310589 198.110326,164.667963 C191.183787,168.842556 183.854737,172.420929 176.223542,175.320965 C180.230393,183.341335 184.861538,190.991831 190.096624,198.16893 C211.238746,191.588051 232.743023,181.531619 254.911949,164.955721 C260.227747,108.668201 245.831087,59.8662432 216.856339,16.5966031 Z M85.4738752,135.09489 C72.8290281,135.09489 62.4592217,123.290155 62.4592217,108.914901 C62.4592217,94.5396472 72.607595,82.7145587 85.4738752,82.7145587 C98.3405064,82.7145587 108.709962,94.5189427 108.488529,108.914901 C108.508531,123.290155 98.3405064,135.09489 85.4738752,135.09489 Z M170.525237,135.09489 C157.88039,135.09489 147.510584,123.290155 147.510584,108.914901 C147.510584,94.5396472 157.658606,82.7145587 170.525237,82.7145587 C183.391518,82.7145587 193.761324,94.5189427 193.539891,108.914901 C193.539891,123.290155 183.391518,135.09489 170.525237,135.09489 Z" fill-rule="nonzero"></path>
+                  </g>
+                </svg>
                 <p>ruan.ritah</p>
               </span>
-              <span className="block">
+              <span className="p-2 text-gray flex align-middle">
+                <EnvelopeIcon width={32} className="mr-2 text-gray"/>
                 <p>ruan.ritah@gamil.com</p>
               </span>
             </div>
           </section>
         </section>
-          
       </div>
-    </main>
+
+      <section className="border-t border-gray py-16">
+        <div className="max-w-7xl mx-auto w-full flex flex-col px-2 sm:px-6 lg:px-8">
+          <div className="flex justify-between">
+            <div>
+              <h1 className="text-white text-xl inline mr-5">RuanRita</h1>
+              <span className="text-gray">ruan.ritah@gmail.com</span>
+              <p className="text-white mt-2">Back-end developer and Front-end developer</p>
+            </div>
+            <div>
+              <h1 className="text-white text-2xl">Media</h1>
+              <div className="flex gap-3 mt-5">
+                <svg width="32px" height="32px" viewBox="0 -28.5 256 256" xmlns="http://www.w3.org/2000/svg"  className="mr-2 fill-gray">
+                  <g>
+                    <path d="M216.856339,16.5966031 C200.285002,8.84328665 182.566144,3.2084988 164.041564,0 C161.766523,4.11318106 159.108624,9.64549908 157.276099,14.0464379 C137.583995,11.0849896 118.072967,11.0849896 98.7430163,14.0464379 C96.9108417,9.64549908 94.1925838,4.11318106 91.8971895,0 C73.3526068,3.2084988 55.6133949,8.86399117 39.0420583,16.6376612 C5.61752293,67.146514 -3.4433191,116.400813 1.08711069,164.955721 C23.2560196,181.510915 44.7403634,191.567697 65.8621325,198.148576 C71.0772151,190.971126 75.7283628,183.341335 79.7352139,175.300261 C72.104019,172.400575 64.7949724,168.822202 57.8887866,164.667963 C59.7209612,163.310589 61.5131304,161.891452 63.2445898,160.431257 C105.36741,180.133187 151.134928,180.133187 192.754523,160.431257 C194.506336,161.891452 196.298154,163.310589 198.110326,164.667963 C191.183787,168.842556 183.854737,172.420929 176.223542,175.320965 C180.230393,183.341335 184.861538,190.991831 190.096624,198.16893 C211.238746,191.588051 232.743023,181.531619 254.911949,164.955721 C260.227747,108.668201 245.831087,59.8662432 216.856339,16.5966031 Z M85.4738752,135.09489 C72.8290281,135.09489 62.4592217,123.290155 62.4592217,108.914901 C62.4592217,94.5396472 72.607595,82.7145587 85.4738752,82.7145587 C98.3405064,82.7145587 108.709962,94.5189427 108.488529,108.914901 C108.508531,123.290155 98.3405064,135.09489 85.4738752,135.09489 Z M170.525237,135.09489 C157.88039,135.09489 147.510584,123.290155 147.510584,108.914901 C147.510584,94.5396472 157.658606,82.7145587 170.525237,82.7145587 C183.391518,82.7145587 193.761324,94.5189427 193.539891,108.914901 C193.539891,123.290155 183.391518,135.09489 170.525237,135.09489 Z" fill-rule="nonzero"></path>
+                  </g>
+                </svg>
+                <svg width="32px" height="32px" viewBox="0 -28.5 256 256" xmlns="http://www.w3.org/2000/svg"  className="mr-2 fill-gray">
+                  <g>
+                    <path d="M216.856339,16.5966031 C200.285002,8.84328665 182.566144,3.2084988 164.041564,0 C161.766523,4.11318106 159.108624,9.64549908 157.276099,14.0464379 C137.583995,11.0849896 118.072967,11.0849896 98.7430163,14.0464379 C96.9108417,9.64549908 94.1925838,4.11318106 91.8971895,0 C73.3526068,3.2084988 55.6133949,8.86399117 39.0420583,16.6376612 C5.61752293,67.146514 -3.4433191,116.400813 1.08711069,164.955721 C23.2560196,181.510915 44.7403634,191.567697 65.8621325,198.148576 C71.0772151,190.971126 75.7283628,183.341335 79.7352139,175.300261 C72.104019,172.400575 64.7949724,168.822202 57.8887866,164.667963 C59.7209612,163.310589 61.5131304,161.891452 63.2445898,160.431257 C105.36741,180.133187 151.134928,180.133187 192.754523,160.431257 C194.506336,161.891452 196.298154,163.310589 198.110326,164.667963 C191.183787,168.842556 183.854737,172.420929 176.223542,175.320965 C180.230393,183.341335 184.861538,190.991831 190.096624,198.16893 C211.238746,191.588051 232.743023,181.531619 254.911949,164.955721 C260.227747,108.668201 245.831087,59.8662432 216.856339,16.5966031 Z M85.4738752,135.09489 C72.8290281,135.09489 62.4592217,123.290155 62.4592217,108.914901 C62.4592217,94.5396472 72.607595,82.7145587 85.4738752,82.7145587 C98.3405064,82.7145587 108.709962,94.5189427 108.488529,108.914901 C108.508531,123.290155 98.3405064,135.09489 85.4738752,135.09489 Z M170.525237,135.09489 C157.88039,135.09489 147.510584,123.290155 147.510584,108.914901 C147.510584,94.5396472 157.658606,82.7145587 170.525237,82.7145587 C183.391518,82.7145587 193.761324,94.5189427 193.539891,108.914901 C193.539891,123.290155 183.391518,135.09489 170.525237,135.09489 Z" fill-rule="nonzero"></path>
+                  </g>
+                </svg>
+                <svg width="32px" height="32px" viewBox="0 -28.5 256 256" xmlns="http://www.w3.org/2000/svg"  className="mr-2 fill-gray">
+                  <g>
+                    <path d="M216.856339,16.5966031 C200.285002,8.84328665 182.566144,3.2084988 164.041564,0 C161.766523,4.11318106 159.108624,9.64549908 157.276099,14.0464379 C137.583995,11.0849896 118.072967,11.0849896 98.7430163,14.0464379 C96.9108417,9.64549908 94.1925838,4.11318106 91.8971895,0 C73.3526068,3.2084988 55.6133949,8.86399117 39.0420583,16.6376612 C5.61752293,67.146514 -3.4433191,116.400813 1.08711069,164.955721 C23.2560196,181.510915 44.7403634,191.567697 65.8621325,198.148576 C71.0772151,190.971126 75.7283628,183.341335 79.7352139,175.300261 C72.104019,172.400575 64.7949724,168.822202 57.8887866,164.667963 C59.7209612,163.310589 61.5131304,161.891452 63.2445898,160.431257 C105.36741,180.133187 151.134928,180.133187 192.754523,160.431257 C194.506336,161.891452 196.298154,163.310589 198.110326,164.667963 C191.183787,168.842556 183.854737,172.420929 176.223542,175.320965 C180.230393,183.341335 184.861538,190.991831 190.096624,198.16893 C211.238746,191.588051 232.743023,181.531619 254.911949,164.955721 C260.227747,108.668201 245.831087,59.8662432 216.856339,16.5966031 Z M85.4738752,135.09489 C72.8290281,135.09489 62.4592217,123.290155 62.4592217,108.914901 C62.4592217,94.5396472 72.607595,82.7145587 85.4738752,82.7145587 C98.3405064,82.7145587 108.709962,94.5189427 108.488529,108.914901 C108.508531,123.290155 98.3405064,135.09489 85.4738752,135.09489 Z M170.525237,135.09489 C157.88039,135.09489 147.510584,123.290155 147.510584,108.914901 C147.510584,94.5396472 157.658606,82.7145587 170.525237,82.7145587 C183.391518,82.7145587 193.761324,94.5189427 193.539891,108.914901 C193.539891,123.290155 183.391518,135.09489 170.525237,135.09489 Z" fill-rule="nonzero"></path>
+                  </g>
+                </svg>
+              </div>
+              
+            </div>
+          </div>
+          <h1 className="text-center text-gray text-lg mt-16">© Copyright 2022. Made by RuanRita</h1>
+        </div>
+      </section>
+    </main> 
   );
 }
